6be5ae74d2afaeef5136ec1b2f72f3abf0680a0f
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-08 11:22:36 -0600

    Handlebars.main starter code

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', apiRoutes);
-router.use('/api', apiRoutes);
-module.exports = router;
--- db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS chicago_db;
-CREATE DATABASE chicago_db;
