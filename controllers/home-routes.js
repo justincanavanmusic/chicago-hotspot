cfdc38c5a97bafb64bdcf5a8a857292ce9961b0d
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-08 11:32:47 -0600

    quick pull

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-// const { Attractions, Users, Reviews }   = require('../models');
+const { Attractions, Users, Reviews }   = require('../models');
 
-// router.get('/', async (req, res) => {
-//     const attractionData = await Attraction.findAll().catch((err) => { 
-//         res.json(err);
-//       });
-//         const attractions = attractionData.map((attraction) => attraction.get({ plain: true }));
-//         res.render('all', { attractions });
-//       });
+router.get('/', async (req, res) => {
+    const attractionData = await Attraction.findAll().catch((err) => { 
+        res.json(err);
+      });
+        const attractions = attractionData.map((attraction) => attraction.get({ plain: true }));
+        res.render('all', { attractions });
+      });
   
 router.get('/', async (req, res) => {
         res.send('all');
