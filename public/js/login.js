5ba66bb05e56eb069999856ecceff74e3d3fa069
Matt Osborne <MattJOzzy@gmail.com>
2023-03-09 12:02:13 -0600

    worked on login/logout scripts

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,49 @@
-// const loginFormHandler = async (event) => {
-//     event.preventDefault();
-  
-//     const username = document.querySelector('#email-login').value.trim();
-//     const password = document.querySelector('#password-login').value.trim();
-  
-//     if (username && password) {
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ username, password }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/');
-//       } else {
-//         alert('Failed to log in.');
-//       }
-//     }
-//   };
-  
-//   document
-//     .querySelector('.login-form')
-//     .addEventListener('submit', loginFormHandler);
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#loginUsername').value.trim();
+    const password = document.querySelector('#loginPassword').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
+const signupFormHandler = async (event) => {
+    event.preventDefault()
+
+    const username = document.querySelector('#signupUsername').value.trim();
+    const password = document.querySelector('#signupPassword').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+document
+    .querySelector('.loginForm')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signupForm')
+    .addEventListener('submit', signupFormHandler);
