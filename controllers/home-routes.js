3b2f6bb6e1c2c225c82e901c6d19ae0706c500ef
allisonnault <allisonrnault@gmail.com>
2023-03-09 13:39:58 -0600

    saving for pull request

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,7 +27,7 @@ router.get('/profile', async (req, res) => {
     });
     const reviews = userReview.map((data) => data.get({ plain: true }));
     console.log(reviews);
-    res.render('profile', { reviews, loggedIn: req.session,loggedIn })
+    res.render('profile', { profile, loggedIn: req.session,loggedIn })
   } catch (err) {
     res.redirect('login');
   }
