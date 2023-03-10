50b1d911066ab409b8c8f6af72067024209a00c5
allisonnault <allisonrnault@gmail.com>
2023-03-10 13:29:34 -0600

    edit review modal working

--- public/js/attractions.js
+++
@@ -1,9 +0,0 @@
-// const attractionCard = document.querySelectorAll('.card');
-
-// attractionCard.forEach((card) => {
-//     const id = card.getAttribute('id');
-// })
-
-// const attraction = attractions.find((attraction) => {
-//     return `card-${attraction.id}` === id;
-// });
