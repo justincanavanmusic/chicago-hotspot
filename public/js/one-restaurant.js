98939c101598b96402bfdfad2eba38c7164b7e4b
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-10 15:33:05 -0600

    Update

--- public/js/one-restaurant.js
+++ public/js/one-restaurant.js
@@ -1,14 +1,22 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
+const commentsContainer = document.getElementById("comments");
+const comments = [];
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
-  const username = document.querySelector('#username').value;
-  const comment = document.querySelector('#comment').value;
 
-  // TODO: Handle form submission (e.g. send data to server)
+  const username = document.querySelector("#username").value;
+  const commentInput = document.querySelector("#comment").value;
 
-  // Optional: clear input fields after form submission
-  document.querySelector('#username').value = '';
-  document.querySelector('#comment').value = '';
+  // Save the comment in an array
+  comments.push({ username, comment: commentInput });
+
+  // Clear input fields after form submission
+  document.querySelector("#username").value = "";
+  document.querySelector("#comment").value = "";
+
+  // Create a new div for the comment and append it to the comments container
+  const commentDiv = document.createElement("div");
+  commentDiv.innerHTML = `<h3>${username}</h3><p>${commentInput}</p>`;
+  commentsContainer.appendChild(commentDiv);
 });
