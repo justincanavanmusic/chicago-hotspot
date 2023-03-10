53b466744f43211dd1a17e3710b233d90f74001b
allisonnault <allisonrnault@gmail.com>
2023-03-10 15:10:55 -0600

    combined get routes for attraction page

--- controllers/api/attraction-routes.js
+++ controllers/api/attraction-routes.js
@@ -2,24 +2,23 @@ const router = require('express').Router();
 const { Attractions, Users, Reviews } = require('../../models');
 const withAuth = require('../utils/auth');
 
+// * /api/attractions
+
 router.get('/:id', withAuth, async (req, res) => {
     try {
         const attractionData = await Attractions.findByPk(req.params.id, {
             include: [
                 {
-                    model: Reviews,
-                    attributes: [
-                        'id',
-                        'body'
-                    ]
+                    model: Reviews, 
+                    include: Users 
                 }
+                
             ]
         })
         const attraction = attractionData.get({ plain: true });
         res.render('one-restaurant', { attraction, loggedIn: req.session.loggedIn });
-        console.log(attractionData)
-        // console.log(attraction.reviews[0].body)
-        // console.log(attraction.reviews[0].id)
+        console.log(attraction)
+   
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
