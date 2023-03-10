fa271d7460268761a7e40f1eb7d78e46dcf6f946
allisonnault <allisonrnault@gmail.com>
2023-03-10 14:27:41 -0600

    working on edit reviews

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -2,7 +2,18 @@ const router = require('express').Router();
 const { Attractions, Users, Reviews } = require('../../models');
 // * /api/reviews
 
+// edit review
+router.put('/:id', (req, res) => {
+    Reviews.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+    })
+    .then(review => res.json({ msg: "updated review" }))
+    .catch(err =>
+        res.status(400).json({ error: "unable to update" }))
 
+});
 
 // delete a review
 
