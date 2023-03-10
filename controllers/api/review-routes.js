abd7902f7826f4f6f77226bed1184f34affd02fd
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-03-10 14:40:16 -0600

    data displaying on one-rest page, working on del route

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -3,7 +3,25 @@ const { Attractions, Users, Reviews } = require('../../models');
 // * /api/reviews
 
 
-
+router.delete('/:id', async (req, res) => {
+    try {
+      const reviewData = await Reviews.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!reviewData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(reviewData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 // delete a review
 
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 
 router.get('/', withAuth, async (req, res) => {
-  const attractionData = await Reviews.findAll().catch((err) => {
+  const attractionData = await Attractions.findAll().catch((err) => {
     res.json(err);
   });
   const attractions = attractionData.map((attraction) => attraction.get({ plain: true }));
@@ -48,7 +48,7 @@ router.get('/profile', withAuth, async (req, res) => {
         user_id: req.session.userId,
       },
       include: [{
-        model: Reviews,
+        model: Attractions,
         required: true
       }]
 
