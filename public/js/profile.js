02cb909969763bd42ed2b0542a923ed22aada72a
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-05-29 20:25:02 -0500

    fixed edit review bug

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,39 +1,83 @@
 // edit review modal
-$('.editReviewForm').on('submit', (e) => {
-    e.preventDefault();
-    const id = e.target.dataset.id;
-    const body = $(`#editBody${id}`).val().trim();
+// $('.editReviewForm').on('submit', (e) => {
+//     e.preventDefault();
+//     const id = e.target.dataset.id;
+//     const body = $(`#editBody${id}`).val().trim();
 
-    fetch(`/api/reviews/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ id, body }),
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
+//     fetch(`/api/reviews/${id}`, {
+//         method: 'PUT',
+//         body: JSON.stringify({ id, body }),
+//         headers: { 'Content-Type': 'application/json' },
+//     })
+//     .then(res => res.json())
+//     window.location.reload();
+//     // window.location.replace('/profile');
+// });
+
+const editBtn = async (e) => {
+  if (e.target.hasAttribute(data-id)) {
+    const id = e.target.hasAttribute("data-id");
+
+    const res = await fetch(`api/reviews/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ id, body }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => res.json());
     window.location.reload();
-    window.location.replace('/profile');
-});
-
-// delete review 
-const deleteButton = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/reviews/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        window.location.reload();
-      } else {
-        alert('Failed to delete post');
-      }
-    }
-  };
-  
-// DELETE BUTTON
- var buttonEls = document
-  .querySelectorAll('.del-button')
-  for (let i = 0; i < buttonEls.length; i++) {
-  buttonEls[i].addEventListener('click', deleteButton);
   }
+};
+
+// delete review
+const deleteButton = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/reviews/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      window.location.reload();
+    } else {
+      alert("Failed to delete post");
+    }
+  }
+};
+
+const saveHandler = async (e) => {
+  // e.preventDefault();
+ 
+    const inputId = document.querySelector('.form-input')
+    .getAttribute('data-id')
+
+    console.log(inputId)
+
+    const body = document.querySelector('.form-input').value;
+    console.log('body = ' + body);
+ 
+
+    const res = await fetch(`/api/reviews/${inputId}`, {
+      method: 'PUT',
+      body: JSON.stringify({ inputId, body }),
+              headers: { 'Content-Type': 'application/json' },
+      
+    })
+
+.then(res => res.json())
+console.log(res);
+
+  window.location.reload();
+
+}
+
+let editBtns = document.querySelectorAll(".edit-post");
+editBtns.forEach((btn) => btn.addEventListener("click", editBtn));
+
+// DELETE BUTTON
+var buttonEls = document.querySelectorAll(".del-button");
+for (let i = 0; i < buttonEls.length; i++) {
+  buttonEls[i].addEventListener('click', deleteButton);
+}
+
+let saveBtns = document.querySelectorAll('.save-btn');
+saveBtns.forEach((btn) => btn.addEventListener('click', saveHandler))
