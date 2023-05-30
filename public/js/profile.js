1aec38facc81e886406cbec0a783d35471355cc5
justincanavanmusic <121266850+justincanavanmusic@users.noreply.github.com>
2023-05-29 20:51:39 -0500

    fixed edit buttons to work on each review

--- public/js/profile.js
+++ public/js/profile.js
@@ -15,7 +15,7 @@
 // });
 
 const editBtn = async (e) => {
-  if (e.target.hasAttribute(data-id)) {
+  if (e.target.hasAttribute(data - id)) {
     const id = e.target.hasAttribute("data-id");
 
     const res = await fetch(`api/reviews/${id}`, {
@@ -45,30 +45,31 @@ const deleteButton = async (event) => {
 };
 
 const saveHandler = async (e) => {
-  // e.preventDefault();
- 
-    const inputId = document.querySelector('.form-input')
-    .getAttribute('data-id')
+  e.preventDefault();
+  if (e.target.hasAttribute("data-id")) {
 
-    console.log(inputId)
+    console.log(e.target)
 
-    const body = document.querySelector('.form-input').value;
-    console.log('body = ' + body);
- 
+  // const id = document.querySelector(".form-input").getAttribute("data-id");
+  const id = e.target.getAttribute('data-id')
 
-    const res = await fetch(`/api/reviews/${inputId}`, {
-      method: 'PUT',
-      body: JSON.stringify({ inputId, body }),
-              headers: { 'Content-Type': 'application/json' },
-      
-    })
+  console.log(id);
 
-.then(res => res.json())
-console.log(res);
+  // const body = document.querySelector(".form-input").value;
+  const body = document.querySelector(`#editBody${id}`).value;
+
+  console.log("body = " + body);
+
+  const res = await fetch(`/api/reviews/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ id, body }),
+    headers: { "Content-Type": "application/json" },
+  }).then((res) => res.json());
+  console.log(res);
 
   window.location.reload();
-
 }
+};
 
 let editBtns = document.querySelectorAll(".edit-post");
 editBtns.forEach((btn) => btn.addEventListener("click", editBtn));
@@ -76,8 +77,8 @@ editBtns.forEach((btn) => btn.addEventListener("click", editBtn));
 // DELETE BUTTON
 var buttonEls = document.querySelectorAll(".del-button");
 for (let i = 0; i < buttonEls.length; i++) {
-  buttonEls[i].addEventListener('click', deleteButton);
+  buttonEls[i].addEventListener("click", deleteButton);
 }
 
-let saveBtns = document.querySelectorAll('.save-btn');
-saveBtns.forEach((btn) => btn.addEventListener('click', saveHandler))
+let saveBtns = document.querySelectorAll(".save-btn");
+saveBtns.forEach((btn) => btn.addEventListener("click", saveHandler));
